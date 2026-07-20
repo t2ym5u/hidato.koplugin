b5c44d13b273fbc6dd1f8fefd2c4011efb8d5775
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:18:36 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,5 @@
+return {
+    ["%1\xC3\x97%2 (1\xE2\x80\x93%3) \xC2\xB7 %4 \xC2\xB7 Empty: %5"] = { fr = "%1\xC3\x97%2 (1\xE2\x80\x93%3) \xC2\xB7 %4 \xC2\xB7 Vides : %5" },
+    ["Connect numbers 1-N on a grid"] = { fr = "Reliez les nombres 1 à N sur une grille" },
+    ["Hidato"] = { fr = "Hidato" },
+}
